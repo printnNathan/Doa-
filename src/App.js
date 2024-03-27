8f8e4e057577baea23565e89f8adca820aef93e5
Nathan Ferreira <nathanferreira2222@gmail.com>
2024-03-26 22:16:48 -0300

    Concluímos o painel

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { View, StyleSheet } from 'react'
 import Navbar from '../src/components/NavBar/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from '../src/components/Footer/footer';
-import Anuncios from '../src/components/Anuncios/anuncios';
+import Anuncios from '../src/components/Anuncios/Anuncios';
 
 function App() {
   return (
--- src/components/Anuncios/anuncios.jsx
+++ src/components/Anuncios/anuncios.jsx
@@ -1,21 +1,48 @@
-import React from 'react';
-import styles from './anuncios.modules.css';
+import { Box, Tab } from '@mui/material';
+import React, { useState } from 'react';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 
-function anuncios() {
+
+function Anuncios() {
+  const [tabSelecionada, setTabSelecionada] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setTabSelecionada(newValue);
+  };
+
   return (
-        <div>
-            <div><h1>Anúncios</h1></div>
-
+    <div>
+      <div><h1>Anúncios</h1></div>
+      {/* 
             <ul className={styles.anuncios}>
                 <li><h2 className={styles.anuncios}>Publicados</h2></li>      
                 <li><h2>Pendentes</h2></li>
                 <li><h2>Inativos</h2></li>
                 <li><h2>Expirados</h2></li>
-            </ul>
-        </div>
+            </ul> */}
+
+      <Box sx={{ width: '100%', typography: 'body1' }}>
+        <TabContext value={tabSelecionada}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="Publicados" value="1" />
+              <Tab label="Pendentes" value="2" />
+              <Tab label="Inativos" value="3" />
+              <Tab label="Expirados" value="4" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">Publicados</TabPanel>
+          <TabPanel value="2">Item Two</TabPanel>
+          <TabPanel value="3">Item Three</TabPanel>
+          <TabPanel value="4">Item Four</TabPanel>
+        </TabContext>
+      </Box>
+    </div>
   );
 }
 
 
-export default anuncios;
+export default Anuncios;
