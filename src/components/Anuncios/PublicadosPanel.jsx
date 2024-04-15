b5c343939895ba0a4413ffaba945f9c345d661b6
Nathan Ferreira <nathanferreira2222@gmail.com>
2024-04-15 12:59:43 -0300

    Iniciando processo de requisição das doações

--- src/components/Anuncios/PublicadosPanel.jsx
+++ src/components/Anuncios/PublicadosPanel.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import ApiService from '../../Services/ApiService';
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
 
 function PublicadosPanel() {
+  const [publicadosPanel, setPublicadosPanel] = useState([]);
+
+  useEffect(() => {
+    ListarProdutos();
+  }, []);
+
+  async function ListarProdutos() {
+    try {
+      const response = await ApiService.get("/Usuarios/Listarprodutospanel");
+
+      if (response.status !== 200) {
+        alert('Erro ao listar produtos');
+        return;
+      }
+      setPublicadosPanel(response.data);
+    } catch (error) {
+      console.error('Erro ao listar produtos:', error);
+      alert('Erro ao listar produtos');
+    }
+  }
+
   return (
     <div>
-      publicados 
+      <h1>Produtos Indisponíveis</h1>
+      <ul>
+        {publicadosPanel.map(produto => (
+          <li key={produto.id}>
+            <h2>{produto.nome}</h2>
+            <p>{produto.descricao}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
