7dc69b33d873896153f5fb88caafc4c2d193a244
Paulo <Paulo>
2024-05-02 22:12:07 -0300

    Corrigindo Arquivos

--- src/components/Anuncios/Anuncios.jsx
+++ src/components/Anuncios/Anuncios.jsx
@@ -5,9 +5,7 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import PublicadosPanel from './PublicadosPanel';
-import PendentesPanel from './PendentesPanel';
 import InativosPanel from './InativosPanel';
-import ExpiradosPanel from './ExpiradosPanel';
 import Styles from '../Anuncios/Anuncios.module.css';
 import Esferas from  '../../components/Esferas/Esferas2';
 import Footer from '../Footer/Footer';
@@ -32,17 +30,13 @@ function Anuncios() {
           <Box sx={{ borderBottom: 1, borderColor: 'divider' }} className={Styles.Painel}>
             <TabList onChange={handleChange} aria-label="lab API tabs example">
               <Tab label="Publicados" value="1" />
-              <Tab label="Pendentes" value="2" />
               <Tab label="Inativos" value="3" />
-              <Tab label="Expirados" value="4" />
             </TabList>
           </Box>
           <Box sx={{ backgroundColor: "#d9d9d9", paddingLeft: "20%", paddingRight: "20%"  }} >
 
             <TabPanel value="1"><PublicadosPanel /></TabPanel>
-            <TabPanel value="2"><PendentesPanel /></TabPanel>
             <TabPanel value="3"><InativosPanel /></TabPanel>
-            <TabPanel value="4"><ExpiradosPanel /></TabPanel>
 
           </Box>
         </TabContext>
