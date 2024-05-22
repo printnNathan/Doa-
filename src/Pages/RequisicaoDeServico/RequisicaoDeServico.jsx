d2a9d560e6bcdd9d13076e00b7b41f7b902e4093
Nathan Ferreira <nathanferreira2222@gmail.com>
2024-05-21 23:47:13 -0300

    Subindo requisição de serviço

--- src/Pages/RequisicaoDeServico/RequisicaoDeServico.jsx
+++ src/Pages/RequisicaoDeServico/RequisicaoDeServico.jsx
@@ -1,86 +1,76 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Requisicaodeservico.module.css';
 import Esferas from "../../components/Esferas/Esferas";
 import Footer from '../../components/Footer/Footer';
 import NavBar from '../../components/NavBar/Header';
 import ApiService from "../../Services/ApiService";
 import ToastService from "../../Services/ToastService";
+import AuthService from "../../Services/AuthService";
 
 const RequisicaoDeServico = () => {
-    const navigate = useNavigate();
-    const [titulo, setTitulo] = useState('');
-    const [descricao, setDescricao] = useState('');
-    const [cep, setCep] = useState('');
-    const [idTipoDoacao, setIdTipoDoacao] = useState('');
-    const [nomeTipoDoacao, setNomeTipoDoacao] = useState('');
-    const [idONG, setIdONG] = useState('');
-    const [nomeONG, setNomeONG] = useState('');
-    const [emailONG, setEmailONG] = useState('');
-    const [senhaONG, setSenhaONG] = useState('');
-    const [imagensPedido, setImagensPedido] = useState([]);
-    const [previa, setPrevia] = useState([]);
-    const tipoTela = 'RequisicaoDeServico'; 
+
 
     function handleFileChange(event) {
         const selectedFiles = event.target.files;
-        if (selectedFiles && selectedFiles.length > 0) {
-            const previews = [];
-            const images = [];
-       
 
-            for (let i = 0; i < selectedFiles.length; i++) {
+        if (selectedFiles.length > 0) {
+            const previas = [];
+            const imagens = [];
+
+            Array.from(selectedFiles).forEach(file => {
+                previas.push(URL.createObjectURL(file));
+
                 const reader = new FileReader();
-                const currentFile = selectedFiles[i];
-
                 reader.onload = function (e) {
-                    previews.push(URL.createObjectURL(currentFile));
-                    images.push({ base64: e.target.result.split(',')[1] });
+                    const base64 = e.target.result.split(',')[1];
+                    imagens.push(base64);
 
-                    if (previews.length === selectedFiles.length) {
-                        setPrevia(previews);
-                        setImagensPedido(images);
+                    // Atualize o estado após todos os arquivos serem processados
+                    if (imagens.length === selectedFiles.length) {
+                        setImagens(imagens);
                     }
                 };
-
-                reader.readAsDataURL(currentFile);
-            }
-        } else {
-            setImagensPedido([]);
-            setPrevia([]);
-        }
-    };
-    //Adicionar função para o mensagem de sucesso aparecer e direcionar para outra tela
-
-
-
-
-
-
-
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const response = await ApiService.post('/PedidosDoacao/cadastrarpedido', {
-                titulo,
-                descricao,
-                cep,
-                iD_ONG: parseInt(idONG),
-                iD_Tipo: parseInt(idTipoDoacao),
-                ong: { 
-                    nome: nomeONG,
-                    email: emailONG,
-                    senha: senhaONG
-                },
-                tipoDoacao: { nome: nomeTipoDoacao } 
+                reader.readAsDataURL(file);
             });
-            console.log('Pedido enviado com sucesso:', response.data);
-            ToastService.Success("Pedido enviado com sucesso!");
-            navigate('/MeusAnuncios');
+
+            console.log(imagens)
+            setPrevia(previas);
+        } else {
+            setImagens(null);
+            setPrevia(null); // Limpa as imagens e previas se nenhum arquivo estiver selecionado
+        }
+    }
+
+
+    const [titulo, setTitulo] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [imagens, setImagens] = useState("");
+    const [previa, setPrevia] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const idOng = AuthService.PegarDadosUsuario().ID;
+            console.log(idOng);
+            const imagensPedido = [];
+
+            imagens.forEach(imagem => {
+                imagensPedido.push({ base64: imagem })
+            });
+
+            const body = {
+                titulo,
+                id_tipo: 2,
+                descricao,
+                id_ong: idOng,
+                status: true,
+                imagensPedido
+            };
+
+            await ApiService.post("/PedidosDoacao/cadastrarpedido", body);
         } catch (error) {
-            console.error('Erro ao enviar pedido:', error);
-            ToastService.Error("Erro ao enviar pedido. Por favor, tente novamente.");
+            ToastService.Error("Erro ao cadastrar Pedido");
         }
     };
 
@@ -90,69 +80,67 @@ const RequisicaoDeServico = () => {
             <div className={styles.container}>
                 <h1 className={styles.text}>Agora, compartilhe algumas informações sobre seu produto</h1>
                 <div className={styles.form}>
-                    <div  className={styles.barra}>
                     <div className={styles.formGroup}>
                         <span className={styles.label}>TÍTULO:</span>
-                        <input 
-                            type="text" 
-                            placeholder="CONSERTO DE ENCANAMENTO" 
-                            className={styles.input} 
-                            value={titulo} 
-                            onChange={(e) => setTitulo(e.target.value)} 
+                        <input
+                            type="text"
+                            placeholder="CONSERTO DE ENCANAMENTO"
+                            className={styles.input}
+                            value={titulo}
+                            onChange={(e) => setTitulo(e.target.value)}
                         />
                     </div>
                     <div className={styles.formGroup}>
                         <span className={styles.label}>DESCRIÇÃO:</span>
-                        <input 
-                            type="text" 
-                            placeholder="SERVIÇO DOMÉSTICO" 
-                            className={styles.input} 
-                            value={descricao} 
-                            onChange={(e) => setDescricao(e.target.value)} 
+                        <input
+                            type="text"
+                            placeholder="SERVIÇO DOMÉSTICO"
+                            className={styles.input}
+                            value={descricao}
+                            onChange={(e) => setDescricao(e.target.value)}
                         />
+
                     </div>
                     <div className={styles.previewContainer}>
-                        {previa.map((src, index) => (
-                        <img key={index} className={styles.previewImage} src={src} alt={`Preview ${index}`} />
-                        ))}
+                        {previa && (
+                            <img className={styles.previewImage} src={previa} alt={`Preview`} />
+                        )}
                     </div>
                     <div className={styles.formGroup}>
-                        <input 
-                            multiple 
-                            type="file" 
-                            accept="image/jpeg" 
-                            onChange={handleFileChange} 
+                        <input
+                            multiple
+                            type="file"
+                            accept="image/jpeg"
+                            onChange={handleFileChange}
                         />
                     </div>
                     <div className={styles.formGroup}>
                         <form onSubmit={handleSubmit}>
                             <h5 className={styles.label}>CONTATO</h5>
-                            <img 
-                                className={styles.icon} 
-                                src='https://cdn-icons-png.flaticon.com/512/15/15407.png' 
-                                alt="Telefone" 
-                                width={30} 
-                                height={30} 
+                            <img
+                                className={styles.icon}
+                                src='https://cdn-icons-png.flaticon.com/512/15/15407.png'
+                                alt="Telefone"
+                                width={30}
+                                height={30}
                             />
+
                             <span className={styles.contact}>(11) 99144-23541</span>
                             <div className={styles.buttonGroup}>
-                                <button type="submit" className={styles.submitButton}>Concluir</button>
+                                <button type="submit" className={styles.submitButton} onClick={handleSubmit}>Concluir</button>
                                 <Link to="/EscolherCategoria">
                                     <button type="button" className={styles.cancelButton}>Cancelar</button>
                                 </Link>
+
                             </div>
-                            
                         </form>
-                    </div>
                     </div>
                 </div>
             </div>
             <Footer />
-            <Esferas tipoTela={tipoTela} />
-            </div>
+            <Esferas />
+        </div>
     );
 }
 
 export default RequisicaoDeServico;
-
-
--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -9,6 +9,10 @@ const AuthService = {
     PegarToken() {
         return localStorage.getItem("jwt");
     },
+    PegarDadosUsuario() {
+        const token = localStorage.getItem("jwt");
+        return jwtDecode(token);
+    },
     Sair() {
         return localStorage.removeItem("jwt");
     },
